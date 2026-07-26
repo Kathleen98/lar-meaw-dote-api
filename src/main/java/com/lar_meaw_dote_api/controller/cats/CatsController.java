d0b2f82e1b8c0153ef0c65c2bd2018b61caf36e5
Kathleen Santos <kathleen.santos@csacademy.com.br>
2026-07-26 11:49:49 -0300

    feat:decoupling the HTTP call from the business rule

--- src/main/java/com/lar_meaw_dote_api/controller/cats/CatsController.java
+++ src/main/java/com/lar_meaw_dote_api/controller/cats/CatsController.java
@@ -1,5 +1,6 @@
 package com.lar_meaw_dote_api.controller.cats;
 
+import com.lar_meaw_dote_api.service.cats.CatsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cats")
 public class CatsController{
 
+    private final CatsService catsService;
+
+    public CatsController(CatsService catsService){
+        this.catsService = catsService;
+    }
+
     @GetMapping
     public String listCats(){
-        return "Deu certo, gatinhos!";
+        return catsService.message();
     }
 }
---
+++ src/main/java/com/lar_meaw_dote_api/service/cats/CatsService.java
@@ -0,0 +1,11 @@
+package com.lar_meaw_dote_api.service.cats;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CatsService {
+
+    public String message(){
+        return "Olá, gatinhos!";
+    }
+}
